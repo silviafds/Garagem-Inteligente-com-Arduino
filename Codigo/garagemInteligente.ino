021df104b4ba951986cd7b2ee3bedc0558d2482b
silviafds <silviafernandes10@outlook.com.br>
2022-02-04 18:28:56 -0300

    coreção abertura do portão

--- Codigo/garagemInteligente.ino
+++ Codigo/garagemInteligente.ino
@@ -26,6 +26,7 @@ AdafruitIO_Feed *sensorgas = io.feed("sensorgas");
 Ultrasonic sensor(D7, D8); //Trigger, echo
 int distancia; 
 bool detectandoVaga();
+String tst;
 
 /**************************** SENSOR GÁS/FUMAÇA ************************************/
 #define pinMQ2 A0 // DECLARAÇÃO SENSOR GÁS/FUMAÇA
@@ -33,31 +34,31 @@ int NivelMQ2 = 300; // NÍVEL LIMITE GÁS AMBIENTE
 bool detectaGasFumaca();
 
 void setup() {
-    myservo.attach(D1);
-    Serial.begin(9600);
-    while(! Serial);
-    conectaBroker();
+  myservo.attach(D1);
+  Serial.begin(9600);
+  while(! Serial);
+  conectaBroker();
 }
 
 void loop() {
-    byte state = io.run();
-    bool x;
-    bool c;
-    if(state == AIO_NET_DISCONNECTED || state == AIO_DISCONNECTED) {
-        conectaBroker();
-    }
+  byte state = io.run();
+  bool x;
+  bool c;
+  if(state == AIO_NET_DISCONNECTED || state == AIO_DISCONNECTED) {
+    conectaBroker();
+  }
    
-        x = detectandoVaga();
-        detectavaga->save(x);
-        Serial.print(" valor retornado vaga ");
-        Serial.println(x);
+  x = detectandoVaga();
+  detectavaga->save(x);
+  Serial.print(" valor retornado vaga ");
+  Serial.println(x);
     
-        c = detectaGasFumaca();
-        sensorgas->save(c);
-        Serial.print(" valor retornado do gas ");
-        Serial.println(c);
+  c = detectaGasFumaca();
+  sensorgas->save(c);
+  Serial.print(" valor retornado do gas ");
+  Serial.println(c);
 
-    delay(4000);
+  delay(4000);
 }
 
 void AbrindoPortao(AdafruitIO_Data *data) {
@@ -65,12 +66,15 @@ void AbrindoPortao(AdafruitIO_Data *data) {
   Serial.print(data->feedName());
   Serial.print(" : ");
   Serial.println(data->value());
-  
+  tst = data->value();
+ 
   if(data->isTrue()) { //Aciona saída conforme dado recebido
-    myservo.write(180);
+    myservo.write(130);
+    detectaGasFumaca(tst); //passa o status quando o portão foi aberto no portal
   } else {
     myservo.write(0);
   }
+
 }
 
 bool detectandoVaga(){
@@ -83,40 +87,49 @@ bool detectandoVaga(){
   if(distancia <= 5) {
     Serial.println(" Não tem vaga");
     return 1;
-    
   } else {
-        Serial.println(" Tem vaga");
-        return 0;
+      Serial.println(" Tem vaga");
+      return 0;
   }
-
+  
 }
 
-bool detectaGasFumaca() {
+bool detectaGasFumaca(String gatilho) {
   int lendoMQ2 = analogRead(pinMQ2);
-
   Serial.print(" leitura: " );
-  Serial.print(lendoMQ2 );
+  Serial.print(lendoMQ2);
   
-  if(lendoMQ2 > NivelMQ2) { 
-    Serial.println(" Gas detectado");
-    myservo.write(120);
-    return 1;
-
-  } else { 
-        Serial.println(" Gas não detectado");
-        myservo.write(0);
-        return 0;
+  if(gatilho == "true") {
+    
+    if (lendoMQ2 < NivelMQ2) {
+      Serial.println(" SEM GAS");
+      return 0;
+    }
+    
+  } else if (gatilho == "false") {
+      if(lendoMQ2 > NivelMQ2) { 
+        Serial.println(" Gas detectado");
+        myservo.write(120);
+        return 1;
+      } else {
+          Serial.println(" SEM GAS");
+          myservo.write(0);
+          return 0;
+      }
+    
   }
+  
+  return 0;
 }
 
 void conectaBroker(){
-  //mensagem de início
+  //mensagem inicial
   Serial.print("Conectando ao Adafruit IO");
  
-  // chama função para conexão com o io.adafruit.com
+  // chama função de conexão io.adafruit.com
   io.connect();
  
-   // instancia para o recebimento de dados que vem do broker
+   // instancia um novo handler para recepção da mensagem do feed Rele
   abreportao->onMessage(AbrindoPortao);
  
   // Aguarda conexação ser estabelecida
